a6ec0be0c2ac50fc3fbc55622f27ed57e6646adf
Claudio Alberto <caljunior@id.uff.br>
2025-07-28 11:58:54 -0300

    Adicionando classe para tratamento de exceções

--- src/main/java/br/com/itau/desafio/controller/TransacaoController.java
+++ src/main/java/br/com/itau/desafio/controller/TransacaoController.java
@@ -5,7 +5,6 @@ import br.com.itau.desafio.service.TransacaoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
---
+++ src/main/java/br/com/itau/desafio/exception/GlobalExceptionHandler.java
@@ -0,0 +1,5 @@
+package br.com.itau.desafio.exception;
+
+public class GlobalExceptionHandler {
+
+}
